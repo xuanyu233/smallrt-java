bf18e8a40a75d8243021e991a41596b9511ce396
Xuanyu Chen <xychen233@gmail.com>
2018-02-20 19:55:15 -0500

    start over Vec3

--- Vec3.java
+++ Vec3.java
@@ -3,13 +3,11 @@
  */
 public class Vec3 {
 
-    public double x, y, z;
+    public double x;
+    public double y;
+    public double z;
 
     public Vec3() {
-    }
-
-    public Vec3(double a) {
-        this(a, a, a);
     }
 
     public Vec3(double x, double y, double z) {
@@ -18,135 +16,126 @@ public class Vec3 {
         this.z = z;
     }
 
-    public double getValue(int i) {
-        if (i == 0)
-            return x;
-        if (i == 1)
-            return y;
-        if (i == 2)
-            return z;
-        return 0.0;
-    }
-
     public double x() {
-        return x;
+        return this.x;
     }
 
     public double y() {
-        return y;
+        return this.y;
     }
 
     public double z() {
-        return z;
+        return this.z;
     }
 
     public double r() {
-        return x;
+        return this.x;
     }
 
     public double g() {
-        return y;
+        return this.y;
     }
 
     public double b() {
-        return z;
-    }
-
-    public static Vec3 minus(Vec3 v) {
-        return new Vec3(-v.x, -v.y, -v.z);
-    }
-
-    public static Vec3 add(Vec3 v1, Vec3 v2) {
-        return new Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-    }
-
-    public static Vec3 sub(Vec3 v1, Vec3 v2) {
-        return new Vec3(v1.x - v2.x, v1.x - v2.x, v1.z - v2.z);
-    }
-
-    public static Vec3 sub(Vec3 v1, double v){
-        return new Vec3(v1.x-v, v1.y-v,v1.z-v);
-    }
-
-    public static Vec3 sub(double a, Vec3 v){
-        return new Vec3(a-v.x, a-v.y, a-v.z);
-    };
-
-    public static Vec3 mul(Vec3 v1, Vec3 v2) {
-        return new Vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
-    }
-
-    public static Vec3 mul(Vec3 v1, double v) {
-        return new Vec3(v1.x * v, v1.x * v, v1.z * v);
-    }
-
-    public static Vec3 div(Vec3 v1, Vec3 v2) {
-        return new Vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
-    }
-
-    public static Vec3 div(Vec3 v1, double v) {
-        return new Vec3(v1.x / v, v1.y / v, v1.z / v);
-    }
-
-    public static Vec3 div(double v, Vec3 v1){
-        return new Vec3(v/v1.x, v/v1.y, v/v1.z);
-    }
-
-    public static double dot(Vec3 v1, Vec3 v2) {
-        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-    }
-
-    public static Vec3 cross(Vec3 v1, Vec3 v2) {
-        return new Vec3(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
-    }
-
-    public static Vec3 unitVector(Vec3 v){
-        return div(v, v.length());
-    }
-
-    public double dot(Vec3 v) {
-        return this.x * v.x + this.y * v.y + this.z * v.z;
-    }
-
-    public Vec3 cross(Vec3 v) {
-        return new Vec3(this.y * v.z - this.z * v.y, this.z * v.x - this.x * v.z, this.x * v.y - this.y * v.x);
-
-    }
-
-    public String toString() {
-        return "( " + this.x + ", " + this.y + ", " + this.z + " )";
+        return this.z;
     }
 
     public double length() {
         return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
     }
 
-    public double squarLength() {
+    public double squareLength() {
         return this.x * this.x + this.y * this.y + this.z * this.z;
     }
 
-    public Vec3 add(double v){
-        return new Vec3(this.x+v, this.y+v, this.z+v);
+    public static Vec3 add(Vec3 v1, Vec3 v2) {
+        return new Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v1.z);
     }
 
-    public Vec3 sub(double v){
-        return new Vec3(this.x-v, this.y-v, this.z-v);
+    public static Vec3 add(Vec3 v, double value) {
+        return new Vec3(v.x + value, v.y + value, v.z + value);
     }
 
-    public Vec3 mul(double v){
-        return new Vec3(this.x*v, this.y*v,this.z*v);
+    public static Vec3 add(double value, Vec3 v) {
+        return add(value, v);
     }
 
-    public Vec3 div(double v){
-        return new Vec3(this.x/v, this.y/v, this.z/v);
+    public static Vec3 sub(Vec3 v1, Vec3 v2) {
+        return new Vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+    }
+
+    public static Vec3 sub(Vec3 v, double value) {
+        return new Vec3(v.x - value, v.x - value, v.z - value);
+    }
+
+    public static Vec3 sub(double value, Vec3 v) {
+        return new Vec3(value - v.x, value - v.y, value - v.z);
+    }
+
+    public static Vec3 mul(Vec3 v1, Vec3 v2) {
+        return new Vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+    }
+
+    public static Vec3 mul(Vec3 v, double value) {
+        return new Vec3(v.x * value, v.y * value, v.z * value);
+    }
+
+    public static Vec3 mul(double value, Vec3 v) {
+        return mul(v, value);
+    }
+
+    public static Vec3 div(Vec3 v1, Vec3 v2){
+        return new Vec3(v1.x/v2.x, v1.y/v2.y, v1.z/v2.z);
+    }
+
+    public static Vec3 div(Vec3 v, double value){
+        return new Vec3(v.x/value, v.y/value, v.z/value);
+    }
+
+    public static Vec3 div(double value, Vec3 v){
+        return new Vec3(value / v.x, value / v.y, value /v.z);
+    }
+
+    public Vec3 add(Vec3 v) {
+        return new Vec3(this.x + v.x, this.y + v.y, this.z + v.z);
+    }
+
+    public Vec3 add(double value) {
+        return new Vec3(this.x + value, this.y + value, this.z + value);
+    }
+
+    public Vec3 sub(Vec3 v) {
+        return new Vec3(this.x - v.x, this.y - v.y, this.z - v.z);
+    }
+
+    public Vec3 sub(double value) {
+        return new Vec3(this.x - value, this.y - value, this.z - value);
+    }
+
+    public Vec3 mul(Vec3 v) {
+        return new Vec3(this.x * v.x, this.y * v.y, this.z * v.z);
+    }
+
+    public Vec3 mul(double value) {
+        return new Vec3(this.x * value, this.y * value, this.z * value);
+    }
+
+    public Vec3 div(Vec3 v){
+        return new Vec3(this.x/v.x, this.y/v.y, this.z/v.z);
+    }
+
+    public Vec3 div(double value){
+        return new Vec3(this.x/value, this.y/value, this.z/value);
+    }
+
+    public static Vec3 unitVector(Vec3 v){
+        double len = Math.sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+        return new Vec3(v.x/len, v.y/len, v.z/len);
     }
 
     public Vec3 unitVector(){
-        return div(this, this.length());
-    }
-
-    public Vec3 minus(){
-        return new Vec3(-this.x, -this.y, -this.z);
+        double len = Math.sqrt(this.x * this.x + this.y*this.y + this.z*this.z);
+        return new Vec3(this.x/len, this.y/len, this.z/len);
     }
 
 }
